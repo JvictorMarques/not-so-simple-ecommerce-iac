00497452f595c48f5189e05be629c972e97c99de
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-08-12 13:51:23 -0300

    style(networking): remove redundant string interpolation
    - Simplify "${local.name_prefix}" to local.name_prefix in vpc, igw, nat gateway tags

--- modules/networking/internet-gateway.tf
+++ modules/networking/internet-gateway.tf
@@ -2,6 +2,6 @@ resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
   tags = {
-    Name = "${local.name_prefix}"
+    Name = local.name_prefix
   }
 }
--- modules/networking/vpc.tf
+++ modules/networking/vpc.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "this" {
   cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = "${local.name_prefix}"
+    Name = local.name_prefix
   }
 }
