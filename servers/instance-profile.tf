3b3979789e5346d0a98aef7acb2b96fec1b1f528
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-08-18 00:28:32 -0300

    refactor(servers): rename instance profile IAM resources for clarity
    - Rename aws_iam_policy_document, aws_iam_role, and
      aws_iam_role_policy_attachment from "this" to "instance_profile" so
      they read clearly now that the module manages multiple IAM roles

--- servers/instance-profile.tf
+++ servers/instance-profile.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "this" {
+data "aws_iam_policy_document" "instance_profile" {
   statement {
     effect = "Allow"
 
@@ -11,17 +11,17 @@ data "aws_iam_policy_document" "this" {
   }
 }
 
-resource "aws_iam_role" "this" {
+resource "aws_iam_role" "instance_profile" {
   name               = "${var.project_name}-instance-role"
-  assume_role_policy = data.aws_iam_policy_document.this.json
+  assume_role_policy = data.aws_iam_policy_document.instance_profile.json
 }
 
-resource "aws_iam_role_policy_attachment" "this" {
+resource "aws_iam_role_policy_attachment" "instance_profile" {
   count      = length(var.policy_arns)
-  role       = aws_iam_role.this.name
+  role       = aws_iam_role.instance_profile.name
   policy_arn = var.policy_arns[count.index]
 }
 resource "aws_iam_instance_profile" "this" {
   name = "${var.project_name}-instance-profile"
-  role = aws_iam_role.this.name
+  role = aws_iam_role.instance_profile.name
 }
