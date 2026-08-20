c4b1ffd93509206f2a387183f1ffb6b237e76dc0
JvictorMarques <jvictorsilvamarques358@gmail.com>
2026-08-20 20:31:12 -0300

    feat(serveless): add input variables for RDS cluster, proxy, and security groups
    - Add rds_cluster, security_groups_names, db_subnet_group_name, rds_proxy variables
    - Drop unused default from order_topic variable

--- serveless/variables.tf
+++ serveless/variables.tf
@@ -53,10 +53,6 @@ variable "order_topic" {
     name          = string
     subscriptions = list(string)
   })
-  default = {
-    name          = "OrderConfirmedTopic"
-    subscriptions = []
-  }
 }
 
 variable "sns_role_policy" {
@@ -72,5 +68,94 @@ variable "s3_application_bucket" {
   })
   default = {
     name = "nsse-application-bucket"
+  }
+}
+
+variable "rds_cluster" {
+  description = "The configuration for the RDS Aurora Serverless cluster"
+  type = object({
+    cluster_identifier          = string
+    engine                      = string
+    engine_mode                 = string
+    database_name               = string
+    master_username             = string
+    manage_master_user_password = bool
+    storage_encrypted           = bool
+    final_snapshot_identifier   = string
+    availability_zones          = list(string)
+    deletion_protection         = bool
+    serverlessv2_scaling_configuration = object({
+      min_capacity = number
+      max_capacity = number
+    })
+    instances = list(object({
+      instance_class    = string
+      availability_zone = string
+      identifier        = string
+    }))
+  })
+  default = {
+    cluster_identifier          = "nsse-aurora-serverless-cluster"
+    engine                      = "aurora-postgresql"
+    engine_mode                 = "provisioned"
+    database_name               = "notSoSimpleEcommerce"
+    master_username             = "nsseAdmin"
+    manage_master_user_password = true
+    storage_encrypted           = true
+    final_snapshot_identifier   = "nsse-aurora-serverless-cluster-final-snapshot"
+    availability_zones          = ["us-east-1a", "us-east-1b"]
+    deletion_protection         = false
+    serverlessv2_scaling_configuration = {
+      min_capacity = 0.5
+      max_capacity = 1
+    }
+    instances = [
+      {
+        instance_class    = "db.serverless"
+        availability_zone = "us-east-1a"
+        identifier        = "nsse-aurora-serverless-cluster-instance-1a"
+      },
+      {
+        instance_class    = "db.serverless"
+        availability_zone = "us-east-1b"
+        identifier        = "nsse-aurora-serverless-cluster-instance-1b"
+      }
+    ]
+  }
+}
+
+variable "security_groups_names" {
+  description = "Name of the RDS security group"
+  type = object({
+    rds           = string
+    control_plane = string
+    worker_nodes  = string
+  })
+  default = {
+    rds           = "nsse-production-rds-security-group"
+    control_plane = "nsse-default-control-plane-security-group"
+    worker_nodes  = "nsse-default-worker-nodes-security-group"
+  }
+}
+
+variable "db_subnet_group_name" {
+  description = "The name of the RDS DB subnet group"
+  type        = string
+  default     = "nsse"
+}
+
+variable "rds_proxy" {
+  description = "The configuration for the RDS Proxy"
+  type = object({
+    name          = string
+    debug_logging = bool
+    engine_family = string
+    role_name     = string
+  })
+  default = {
+    name          = "nsse-db-proxy"
+    debug_logging = false
+    engine_family = "POSTGRESQL"
+    role_name     = "nsse-rds-proxy-role"
   }
 }
